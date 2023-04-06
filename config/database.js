1299ecdccd99a315460b881e729fe6ff77bb6491
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-06 07:28:27 +0000

    update html engine

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
+      database: env('PGDATABASE', 'strapi_zhai_db'),
       user: env('PGUSER', ''),
       password: env('PGPASSWORD', ''),
       ssl: env.bool(true),
